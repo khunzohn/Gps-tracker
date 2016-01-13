11a3e82ecdbc0f6c7fde402854357dc2fa1ad4b1
khunzohn <khunzohn@gmail.com>
2016-01-13 16:51:02 +0630

    store first app launch flag in shared preference

--- app/src/main/java/com/hilllander/khunzohn/gpstracker/WelcomeActivity.java
+++ app/src/main/java/com/hilllander/khunzohn/gpstracker/WelcomeActivity.java
@@ -2,27 +2,40 @@
 package com.hilllander.khunzohn.gpstracker;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 
 import com.hilllander.khunzohn.gpstracker.util.ViewUtils;
 
 public class WelcomeActivity extends AppCompatActivity {
     private static final long TIME = 2000;
+    private static final String KEY_FIRST_APP_LAUNCH = "key_for_first_app_launch";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
         ViewUtils.setStatusBarTint(this, R.color.colorPrimaryDark);
+        final SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+        final boolean isFirstAppLaunch = sp.getBoolean(KEY_FIRST_APP_LAUNCH, true);
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent market = new Intent(WelcomeActivity.this, MarketingActivity.class);
-                startActivity(market);
-                finish();
+                if (isFirstAppLaunch) {
+                    sp.edit().putBoolean(KEY_FIRST_APP_LAUNCH, false).apply();
+                    Intent market = new Intent(WelcomeActivity.this, MarketingActivity.class);
+                    startActivity(market);
+                    finish();
+                } else {
+                    Intent main = new Intent(WelcomeActivity.this, MainActivity.class);
+                    startActivity(main);
+                    finish();
+                }
+
             }
         }, TIME);
     }
